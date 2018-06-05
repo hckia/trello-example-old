e58d971eb67a10ceac68e7d260ccfc7329618ab1
H. Cyrus Kia <hckia1@gmail.com>
2018-06-04 17:41:50 -0700

    updated trello app with tests.

--- trello/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Board from './components/board'; // Imported the Board component
-import './index.css';
-
-ReactDOM.render(
-  <Board />, 
-  document.getElementById('root')
-);
